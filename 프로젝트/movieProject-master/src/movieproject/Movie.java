4275f9f38f427c6cddf1d750242deb3f1574d55c
EoJY <101792206+EoJY@users.noreply.github.com>
2022-06-13 14:15:08 +0900

    Add files via upload

--- 프로젝트/movieProject-master/src/movieproject/Movie.java
+++ 프로젝트/movieProject-master/src/movieproject/Movie.java
@@ -1,10 +1,17 @@
 package movieproject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Movie {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Mainmenu m = new Mainmenu();
 		m.menuOpen();
@@ -13,56 +20,126 @@ public class Movie {
 }
 interface Menu
 {
-	public void menuOpen();
-	public void movieView();
-	public void Reservation();
-	public void reservationCancel();
+	public void menuOpen() throws IOException;
+	public void movieView() throws IOException;
+	public void Seat();
 	public void Seats();
 	
 }
 
 abstract class AbstractMovie implements Menu
 {
-
+	File file = new File("../movie.txt");
+	Scanner sc = new Scanner(System.in);
+	boolean flag1=true;
+	
 	@Override
-	public void menuOpen() {
+	public void menuOpen() throws IOException {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void movieView() {
+	public void movieView() throws IOException {
 		// TODO Auto-generated method stub
-		
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		String str=null;
+		Adminmenu admin = new Adminmenu();
+		while((str=br.readLine())!=null)
+		{
+			System.out.println(str);
+			
+		}
 	}
 
-	@Override
-	public void Reservation() {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 	@Override
 	public void Seats() {
 		// TODO Auto-generated method stub
+		Commands c = new Commands();
+		c.prints();
+		c.Init_seat();
+		c.Print_seat();
+		c.print();
 		
-	}
-
-	@Override
-	public void reservationCancel() {
-		// TODO Auto-generated method stub
-		
+		while(true)
+		{
+			Scanner sc = new Scanner(System.in);
+			String input = sc.nextLine();
+			
+			input.toUpperCase();
+			try {
+				c.option(input);
+			}catch(ArrayIndexOutOfBoundsException e)
+			{
+				System.out.println("");
+				System.out.println("잘못 입력하셨습니다. 좌석예약은 '대문자' , '숫자' 만 가능합니다 다시 입력하세요.");
+				System.out.println("");
+				c.print();
+			}
+		}
 	}
 	
+	public void movieSet() throws IOException
+	{
+		if(!file.exists())
+		{
+			System.out.println("영화 목록을 만듭니다.");
+			file.createNewFile();
+		}
+		FileWriter fw = new FileWriter(file, true);
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		System.out.println("영화의 제목과 장르를 입력해 주세요.");
+		
+		int plugin = 1;
+		while(plugin==1)
+		{
+			Long time = System.currentTimeMillis();
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("영화 이름을 입력해 주세요.");
+			String moviename = sc.nextLine();
+			System.out.println("영화 장르를 입력해 주세요.");
+			String movietype = sc.nextLine();
+			
+			String str = time+","+moviename+","+movietype;
+			bw.write(str+'\n');
+			System.out.println("영화 등록이 마무리 되었습니다.");
+			
+			System.out.println("계속 등록하고 싶으시면 1, 종료하고 싶으시면 0을 눌러주세요.");
+			plugin=sc.nextInt();
+			if(plugin>1)
+			{
+				System.out.println("잘못된 숫자 입니다.");
+			}
+			
+			
+		}
+		bw.close();
+		
+		
+		FileReader  fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		
+		String read=null;
+		while((read=br.readLine())!=null)
+		{
+			System.out.println(read);
+		}
+	}
 }
 
 
 class Mainmenu extends AbstractMovie
 {
 	Scanner sc = new Scanner(System.in);
-	boolean flag = true;
+	static boolean flag = true;
+	Adminmenu admin = new Adminmenu();
 	@Override
-	public void menuOpen() {
+	public void menuOpen() throws IOException {
 		// TODO Auto-generated method stub
 		
 		System.out.println("===========================");
@@ -76,17 +153,19 @@ class Mainmenu extends AbstractMovie
 			switch(input)
 			{
 			case 1:
-				movieView();
+				super.movieView();
+				System.out.println("1.영화 확인하기 2. 영화 예매하기 3. 예매 취소하기 4.관리자 메뉴가기 5.종료");
 				break;
 			case 2:
-				Reservation();
+				super.Seats();
+				System.out.println("1.영화 확인하기 2. 영화 예매하기 3. 예매 취소하기 4.관리자 메뉴가기 5.종료");
 				break;
 			case 3:
-				ReservationOut();
+				super.Seats();
+				System.out.println("1.영화 확인하기 2. 영화 예매하기 3. 예매 취소하기 4.관리자 메뉴가기 5.종료");
 				break;
 			case 4:
-				callAdminMenu();
-				break;
+				admin.menuOpen();
 			case 5:
 				System.out.println("종료 되었습니다.");
 				flag=false;
@@ -101,60 +180,94 @@ class Mainmenu extends AbstractMovie
 	}
 
 	@Override//영화 메뉴
-	public void movieView() {
+	public void movieView() throws IOException {
 		// TODO Auto-generated method stub
 		super.movieView();
 	}
 
-	@Override
-	public void Reservation() {
-		// TODO Auto-generated method stub
-		super.Reservation();
-		Seats();
-	}
-
 	
-	@Override
 	public void Seats() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void ReservationOut() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public void callAdminMenu()
+	public void callAdminMenu() throws IOException
 	{
 		Adminmenu a = new Adminmenu();
 		a.menuOpen();
+	}
+
+	@Override
+	public void Seat() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
 
 class Adminmenu extends AbstractMovie
 {
-
+	
+	File file = new File("../movie.txt");
+	Scanner sc = new Scanner(System.in);
+	boolean flag1=true;
+	
 	@Override
-	public void menuOpen() {
+	public void menuOpen() throws IOException {
 		// TODO Auto-generated method stub
-		super.menuOpen();
+		System.out.println("===========================");
+		System.out.println("==========관리자매뉴==========");
+		System.out.println("===========================");
+		System.out.println("1.영화 확인하기 2. 영화 등록하기 3. 영화 삭제하기 4.종료");
+		
+		while(flag1)
+		{
+			int input = sc.nextInt();
+			switch(input)
+			{
+			case 1:
+				movieView();
+				System.out.println("1.영화 확인하기 2. 영화 등록하기 3. 영화 삭제하기 4.종료");
+				break;
+			case 2:
+				movieSet();
+				System.out.println("1.영화 확인하기 2. 영화 등록하기 3. 영화 삭제하기 4.종료");
+				break;
+			case 3:
+				movieDelete();
+				break;
+			case 4:
+				System.out.println("종료 되었습니다.");
+				flag1=false;
+				break;
+			default:
+				System.out.println("잘못 입력하셨습니다..");
+				System.out.println("1.영화 확인하기 2. 영화 등록하기 3. 영화 삭제하기 4.종료");
+				break;
+			}
+		}
 	}
 
 	@Override
-	public void movieView() {
+	public void movieView() throws IOException {
 		// TODO Auto-generated method stub
 		super.movieView();
 	}
 
-	public void movieSet()
+	public void movieSet() throws IOException
 	{
+		super.movieSet();
 		
 	}
 	
 	public void movieDelete()
 	{
+		
+	}
+
+	@Override
+	public void Seat() {
+		// TODO Auto-generated method stub
 		
 	}
 	
---
+++ 프로젝트/movieProject-master/src/movieproject/Seats.java
@@ -0,0 +1,148 @@
+package movieproject;
+import java.util.Scanner;
+
+class Commands
+{
+	final int SIZE =10;
+	char[][] seat = new char[SIZE][SIZE];
+	
+	protected void print()
+	{
+		System.out.print("입력>>");
+	}
+	protected void prints()
+	{
+		System.out.println("예약 하실 좌석을 선택하세요");
+		System.out.println("");
+		System.out.println("좌석은 A1~I9까지 가능합니다 ");
+		System.out.println("좌석 예약을 원하실 경우 예시 '+A1'");
+		System.out.println("좌석 취소를 원하실 경우 예시 '-A1'");
+		System.out.println("예약 조회를 원하실 경우 P 를 입력하세요");
+		System.out.println("예약 종료를 원하실 경우 E 를 입력하세요");
+		System.out.println("");
+	}
+	
+	void Init_seat()
+	{
+		char col='A';
+		char row='1';
+		
+		for(int i=1;i<SIZE;i++)
+		{
+			for(int j=0;j<SIZE;j++)
+			{
+				seat[i][j] = 'O';
+			}
+		}
+		for(int i=1;i<SIZE;i++)
+		{
+			seat[i][0]=col++;
+		}
+		for(int j=1;j<SIZE;j++)
+		{
+			seat[0][j]=row++;
+		}
+	}
+	void Print_seat()
+	{
+		for(char [] index : seat)
+		{
+			for(char temp : index)
+			{
+				System.out.printf("%c \t",temp);
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+	}
+	void option(String input) 
+	{
+		char[] ip=input.toCharArray();
+		char op =ip[0];
+		int col=0,row=0;
+		if (ip.length >3)
+		{
+			System.out.println("");
+			System.out.println("잘못 입력하셨습니다. 좌석예약은 '대문자' , '숫자' 만 가능합니다 다시 입력하세요.");
+			System.out.println("");
+			print();
+				return;
+		}
+		if(op=='+'||op=='-')
+		{
+			col=(int)ip[1]-64;
+			row=(int)ip[2]-48;
+			
+		}
+		
+		switch (op)
+		{
+		
+		case'+':
+			if(seat[col][row] !='X')
+			{
+				seat[col][row]='X';
+				System.out.println("");
+				System.out.printf("%c%c좌석을 예매했습니다. %n%n", ip[1], ip[2]);
+			}
+			else
+			{
+				System.out.println("");
+				 System.out.printf("이미 예약이 되어있는 자리 입니다.%n%n");
+			}
+			print();
+				break;
+			
+		case'-':
+			if(seat[col][row] !='O')
+			{
+				seat[col][row]='O';
+				System.out.println("");
+				System.out.printf("%c%c좌석을 취소했습니다. %n%n", ip[1], ip[2]);
+			}
+			else
+			{
+				System.out.println("");
+				System.out.printf("예약 하지않은 자리 입니다.%n%n");
+			}
+			print();
+				break;
+			
+		case 'P':
+		case 'p':
+			if(ip.length >=2)
+			{
+				System.out.println();
+				System.out.printf("잘못 입력하셨습니다. 다시 입력하세요. %n%n");
+				print();
+					return;
+			}
+			Print_seat();
+			System.out.println("조회를 완료하였습니다.");
+			System.out.println("");
+			prints();
+			print();
+				break;
+				
+		case 'E':
+		case 'e':
+			if(ip.length >=2)
+			{
+				System.out.println();
+				System.out.printf("잘못 입력하셨습니다. 다시 입력하세요. %n%n");
+				print();
+					return;
+			}
+			System.out.println("");
+			System.out.print("프로그램을 종료합니다.");
+			System.exit(0);
+				break;
+		
+		default:
+			System.out.println("");
+			System.out.printf("잘못 입력하셨습니다. 다시 입력하세요. %n%n");
+			print();
+				break;
+		}
+	}
+}
